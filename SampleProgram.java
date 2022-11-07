928db9ea62afb87ae622622012945eae23acfab0
Naresh Babu Sure <techsoft912@gmail.com>
2022-11-07 12:01:33 +0530

    Sample Java Program commited first time

---
+++ SampleProgram.java
@@ -0,0 +1,20 @@
+package coreJava;
+
+
+public class FirstJavaProgram
+{	
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is First Java program");
+		m1();
+		//FirstJavaProgram.m1();
+		
+	}
+	
+	public static void m1()
+	{
+		System.out.println("Calling m1() method");
+	}
+
+}
